7b4927187b7b995043959171e9d28b0162d7d012
Stephen Kump <stephen.kump@yale.edu>
2016-10-05 05:30:56 +0000

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/fair-hare', function(request, response){
+    response.send('YOU SHOULD PUT YOUR NICKNAME HERE.');
+});
+
 app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
